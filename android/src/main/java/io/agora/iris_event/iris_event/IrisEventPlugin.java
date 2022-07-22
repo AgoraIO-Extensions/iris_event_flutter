e3ee0ab3998df909f85e427f33add060c965a03f
Littlegnal <8847263+littleGnAl@users.noreply.github.com>
2022-07-22 16:41:00 +0800

    Convert kotlin to java to avoid kotlin version conflict (#2)

---
+++ android/src/main/java/io/agora/iris_event/iris_event/IrisEventPlugin.java
@@ -0,0 +1,17 @@
+package io.agora.iris_event.iris_event;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+
+/** IrisEventPlugin */
+public class IrisEventPlugin implements FlutterPlugin {
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+  }
+}

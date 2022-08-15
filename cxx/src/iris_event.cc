ebdb5c71138ad1e55a3096c62e0a74bcec00c002
littleGnAl <tearjeaker@hotmail.com>
2022-08-15 18:57:01 +0800

    WIP

--- cxx/src/iris_event.cc
+++ cxx/src/iris_event.cc
@@ -32,59 +32,8 @@ EXPORT void OnEvent(const char *event,
                     unsigned int *length,
                     unsigned int buffer_count)
 {
-    if (dart_send_port_ == -1)
-    {
-        return;
-    }
 
-    Dart_CObject c_event;
-    c_event.type = Dart_CObject_kString;
-    c_event.value.as_string = const_cast<char *>(event);
-
-    Dart_CObject c_data;
-    c_data.type = Dart_CObject_kString;
-    c_data.value.as_string = const_cast<char *>(data);
-
-    Dart_CObject **type_data_array =
-        new Dart_CObject *[buffer_count];
-    for (int i = 0; i < buffer_count; i++)
-    {
-        const void *obuffer = buffer[i];
-        unsigned int abufferLength = length[i];
-        uint8_t *abuffer = reinterpret_cast<uint8_t *>(malloc(abufferLength));
-        memcpy(abuffer, obuffer, abufferLength);
-
-        Dart_CObject *cbuffer = new Dart_CObject;
-        cbuffer->type = Dart_CObject_kExternalTypedData;
-        cbuffer->value.as_external_typed_data.type = Dart_TypedData_kUint8;
-        cbuffer->value.as_external_typed_data.length = abufferLength;
-        cbuffer->value.as_external_typed_data.data = abuffer;
-        cbuffer->value.as_external_typed_data.peer = abuffer;
-        cbuffer->value.as_external_typed_data.callback = Finalizer;
-
-        type_data_array[i] = cbuffer;
-    }
-
-    Dart_CObject dbuffer;
-    dbuffer.type = Dart_CObject_kArray;
-    dbuffer.value.as_array.length = buffer_count;
-    dbuffer.value.as_array.values = type_data_array;
-
-    Dart_CObject *c_event_data_arr[] = {&c_event, &c_data, &dbuffer};
-
-    Dart_CObject c_on_event_data;
-    c_on_event_data.type = Dart_CObject_kArray;
-    c_on_event_data.value.as_array.values = c_event_data_arr;
-    c_on_event_data.value.as_array.length =
-        sizeof(c_event_data_arr) / sizeof(c_event_data_arr[0]);
-
-    Dart_PostCObject_DL(dart_send_port_, &c_on_event_data);
-
-    for (intptr_t i = 0; i < buffer_count; ++i)
-    {
-        delete type_data_array[i];
-    }
-    delete[] type_data_array;
+    OnEventEx(event, data, nullptr, buffer, length, buffer_count);
 }
 
 EXPORT void OnEventEx(const char *event, const char *data,
@@ -99,7 +48,7 @@ EXPORT void OnEventEx(const char *event, const char *data,
 
     Dart_CObject c_event;
     c_event.type = Dart_CObject_kString;
-    if (data == nullptr)
+    if (event == nullptr)
     {
         c_event.value.as_string = (char *)"";
     }
@@ -110,46 +59,70 @@ EXPORT void OnEventEx(const char *event, const char *data,
 
     Dart_CObject c_data;
     c_data.type = Dart_CObject_kString;
-    c_data.value.as_string = const_cast<char *>(data);
-
-    Dart_CObject **type_data_array =
-        new Dart_CObject *[buffer_count];
-    for (int i = 0; i < buffer_count; i++)
+    if (data == nullptr)
     {
-        const void *obuffer = buffer[i];
-        unsigned int abufferLength = length[i];
-        uint8_t *abuffer = reinterpret_cast<uint8_t *>(malloc(abufferLength));
-        memcpy(abuffer, obuffer, abufferLength);
-
-        Dart_CObject *cbuffer = new Dart_CObject;
-        cbuffer->type = Dart_CObject_kExternalTypedData;
-        cbuffer->value.as_external_typed_data.type = Dart_TypedData_kUint8;
-        cbuffer->value.as_external_typed_data.length = abufferLength;
-        cbuffer->value.as_external_typed_data.data = abuffer;
-        cbuffer->value.as_external_typed_data.peer = abuffer;
-        cbuffer->value.as_external_typed_data.callback = Finalizer;
-
-        type_data_array[i] = cbuffer;
+        c_data.value.as_string = (char *)"";
+    }
+    else
+    {
+        c_data.value.as_string = const_cast<char *>(data);
     }
 
-    Dart_CObject dbuffer;
-    dbuffer.type = Dart_CObject_kArray;
-    dbuffer.value.as_array.length = buffer_count;
-    dbuffer.value.as_array.values = type_data_array;
-
-    Dart_CObject *c_event_data_arr[] = {&c_event, &c_data, &dbuffer};
-
-    Dart_CObject c_on_event_data;
-    c_on_event_data.type = Dart_CObject_kArray;
-    c_on_event_data.value.as_array.values = c_event_data_arr;
-    c_on_event_data.value.as_array.length =
-        sizeof(c_event_data_arr) / sizeof(c_event_data_arr[0]);
-
-    Dart_PostCObject_DL(dart_send_port_, &c_on_event_data);
-
-    for (intptr_t i = 0; i < buffer_count; ++i)
+    if (buffer_count != 0)
     {
-        delete type_data_array[i];
+        Dart_CObject dbuffer;
+        dbuffer.type = Dart_CObject_kArray;
+        dbuffer.value.as_array.length = buffer_count;
+        Dart_CObject **type_data_array =
+            new Dart_CObject *[buffer_count];
+        for (int i = 0; i < buffer_count; i++)
+        {
+            const void *obuffer = buffer[i];
+            unsigned int abufferLength = length[i];
+            uint8_t *abuffer = reinterpret_cast<uint8_t *>(malloc(abufferLength));
+            memcpy(abuffer, obuffer, abufferLength);
+
+            Dart_CObject *cbuffer = new Dart_CObject;
+            cbuffer->type = Dart_CObject_kExternalTypedData;
+            cbuffer->value.as_external_typed_data.type = Dart_TypedData_kUint8;
+            cbuffer->value.as_external_typed_data.length = abufferLength;
+            cbuffer->value.as_external_typed_data.data = abuffer;
+            cbuffer->value.as_external_typed_data.peer = abuffer;
+            cbuffer->value.as_external_typed_data.callback = Finalizer;
+
+            type_data_array[i] = cbuffer;
+        }
+        dbuffer.value.as_array.values = type_data_array;
+        Dart_CObject *c_event_data_arr[] = {&c_event, &c_data, &dbuffer};
+
+        Dart_CObject c_on_event_data;
+        c_on_event_data.type = Dart_CObject_kArray;
+        c_on_event_data.value.as_array.values = c_event_data_arr;
+        c_on_event_data.value.as_array.length =
+            sizeof(c_event_data_arr) / sizeof(c_event_data_arr[0]);
+
+        Dart_PostCObject_DL(dart_send_port_, &c_on_event_data);
+
+        if (buffer_count != 0)
+        {
+            for (intptr_t i = 0; i < buffer_count; ++i)
+            {
+                delete type_data_array[i];
+            }
+            delete[] type_data_array;
+        }
     }
-    delete[] type_data_array;
+    else
+    {
+        Dart_CObject *c_event_data_arr[] = {&c_event, &c_data};
+
+        Dart_CObject c_on_event_data;
+        c_on_event_data.type = Dart_CObject_kArray;
+        c_on_event_data.value.as_array.values = c_event_data_arr;
+        c_on_event_data.value.as_array.length =
+            sizeof(c_event_data_arr) / sizeof(c_event_data_arr[0]);
+
+        Dart_PostCObject_DL(dart_send_port_, &c_on_event_data);
+    }
+    // dbuffer.value.as_array.values = type_data_array;
 }
--- ios/iris_event.podspec
+++ ios/iris_event.podspec
@@ -16,7 +16,8 @@ A new flutter plugin project.
   s.source_files = 'Classes/**/*'
   s.public_header_files = 'Classes/**/*.h'
   s.dependency 'Flutter'
-  s.dependency 'AgoraIrisEventFlutter_iOS_Beta', '1.0.0'
+  # s.dependency 'AgoraIrisEventFlutter_iOS_Beta', '1.0.0'
+  s.dependency 'iris_event_handler'
   s.platform = :ios, '9.0'
   s.libraries = 'stdc++'
 

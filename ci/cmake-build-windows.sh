79a714f4c19294817fd23aa6edbde065e3b60702
littleGnAl <tearjeaker@hotmail.com>
2022-08-30 21:31:49 +0800

    Add ci build

---
+++ ci/cmake-build-windows.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+SCRIPTS_PATH=$(dirname "$0")
+ROOT_PATH=$(pwd)
+BUILD_TYPE="Release"
+
+OUTPUT_ARTIFACTORY_NAME="Agora_Iris_Event_Flutter_for_Windows"
+OUTPUT_ARTIFACTORY_PATH=$ROOT_PATH/build/$OUTPUT_ARTIFACTORY_NAME
+
+mkdir -p $OUTPUT_ARTIFACTORY_PATH
+
+bash $ROOT_PATH/cxx/build-windows.sh $ROOT_PATH/cxx $OUTPUT_ARTIFACTORY_PATH
+
+ZIP_FILE_NAME="${OUTPUT_ARTIFACTORY_NAME}.zip"
+
+pushd $ROOT_PATH/build
+zip -r "$ZIP_FILE_NAME" $OUTPUT_ARTIFACTORY_NAME
+popd

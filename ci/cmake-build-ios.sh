c5b3b9a37bd96d1791dd4a478acdf377bbf82209
littleGnAl <tearjeaker@hotmail.com>
2022-10-08 13:28:16 +0800

    fix: Fix CFBundleShortVersionString/CFBundleVersion not be set on ios/macos

--- ci/cmake-build-ios.sh
+++ ci/cmake-build-ios.sh
@@ -13,6 +13,20 @@ mkdir -p $OUTPUT_ARTIFACTORY_PATH
 
 bash $ROOT_PATH/cxx/build-ios.sh $ROOT_PATH/cxx $OUTPUT_ARTIFACTORY_PATH
 
+# Check CFBundleShortVersionString
+CFBUNDLESHORTVERSIONSTRING=$(defaults read ${OUTPUT_ARTIFACTORY_PATH}/iris_event_handler.xcframework/ios-arm64_armv7/iris_event_handler.framework/Info.plist CFBundleShortVersionString)
+if [[ -z "${CFBUNDLESHORTVERSIONSTRING}" ]]; then
+    echo "CFBundleShortVersionString not found, make sure the MACOSX_FRAMEWORK_SHORT_VERSION_STRING has been set in the CMakeLists.txt"
+    exit 1
+fi
+
+# Check CFBundleVersion
+CFBUNDLEVERSION=$(defaults read ${OUTPUT_ARTIFACTORY_PATH}/iris_event_handler.xcframework/ios-arm64_armv7/iris_event_handler.framework/Info.plist CFBundleVersion)
+if [[ -z "${CFBUNDLEVERSION}" ]]; then
+    echo "CFBundleVersion not found, make sure the MACOSX_FRAMEWORK_BUNDLE_VERSION has been set in the CMakeLists.txt"
+    exit 1
+fi
+
 ZIP_FILE_NAME="${OUTPUT_ARTIFACTORY_NAME}.zip"
 
 pushd $ROOT_PATH/build
